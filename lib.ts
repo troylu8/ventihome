6991ce96a417627c8b1ea1fe1546bfbaee0b7a26
troy <86700714+troylu8@users.noreply.github.com>
2024-12-31 14:11:22 -0800

    add local cache

--- lib.ts
+++ lib.ts
@@ -1,16 +1,24 @@
 import fs from "fs";
 
-export async function readText(src: string) {
+export const cache = new Map<string, any>();
+
+async function read(src: string, transform: (text: string) => any) {
+    
+    let res = cache.get(src);
+
+    if (res) return res;
+    
     try {
-        return await fs.promises.readFile(
-            `${process.cwd()}/public/${src}`,
-            "utf8"
-        );
+        res = transform(await fs.promises.readFile(`${process.cwd()}/public/${src}`,"utf8"));
+        cache.set(src, res);
+        return res; 
     } catch (e: any) {
         return null;
     }
 }
+export async function readText(src: string) {
+    return read(src, (text) => text);
+}
 export async function readJSON(src: string) {
-    const text = await readText(src);
-    return text? JSON.parse(text) : null;
+    return await read(src, (text) => text? JSON.parse(text) : null);
 }
